1c5acacb7b8fd72380361b6bb17b059e28290908
Nobody <ich@sektor.selfip.com>
2015-06-16 12:49:14 +0200

    Moved word stem tests into own file

---
+++ FrenchVerbWordStemTest.php
@@ -0,0 +1,18 @@
+<?php
+require 'conjugate.php';
+class FrenchVerbWordStemTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider regularWordStemProvider
+	 */
+	public function testWordStem($expectedResult, $verb) {
+		$this->assertEquals ( $expectedResult, word_stem ( $verb ) );
+	}
+	public function regularWordStemProvider() {
+		return array (
+				array (
+						"aim",
+						"aimer"
+				)
+		);
+	}
+}
