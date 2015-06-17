a40a3d4cc16552e5b755604a1dc7b8d53b386caf
GrischaK <grischa.kosba@gmail.com>
2015-06-17 13:37:59 +0200

    added test for conjugation auxiliary

--- AuxiliaireTest.php
+++ AuxiliaireTest.php
@@ -1,6 +1,8 @@
 <?php
 require 'conjugate.php';
 class AuxiliaireTest extends PHPUnit_Framework_TestCase {
+	
+
 	/**
 	 * @dataProvider regularAuxiliaireProvider
 	 */
@@ -15,6 +17,102 @@ class AuxiliaireTest extends PHPUnit_Framework_TestCase {
 				) 
 		);
 	}
+	
+	/**
+	 * @dataProvider AuxiliaireVerbProvider
+	 */
+	public function testAuxiliaireVerb($expectedResult, $verb, $tense, $person, $mood) {
+		$this->assertEquals ( $expectedResult, conjugated_auxiliaire ( $auxiliaire, $person, $tense, $mood ) );
+	}
+	public function AuxiliaireVerbProvider() {
+		return array (
+				array (
+						"suis",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::FirstPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"es",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::SecondPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"est",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::ThirdPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"sommes",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::FirstPersonPlural,
+						Mood::Indicative
+				),
+				array (
+						"êtes",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::SecondPersonPlural,
+						Mood::Indicative
+				),
+				array (
+						"sont",
+						Auxiliaire::Etre,
+						Tense::Passe_compose,
+						Person::ThirdPersonPlural,
+						Mood::Indicative
+				),
+				array (
+						"ai",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::FirstPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"as",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::SecondPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"a",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::ThirdPersonSingular,
+						Mood::Indicative
+				),
+				array (
+						"avons",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::FirstPersonPlural,
+						Mood::Indicative
+				),
+				array (
+						"avez",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::SecondPersonPlural,
+						Mood::Indicative
+				),
+				array (
+						"avont",
+						Auxiliaire::Avoir,
+						Tense::Passe_compose,
+						Person::ThirdPersonPlural,
+						Mood::Indicative
+				)
+		);
+}
+
 }
 
 
