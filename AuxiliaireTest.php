025c2d945d763fc7b9c2200ec984f73846e7867f
GrischaK <grischa.kosba@gmail.com>
2015-06-17 11:38:36 +0200

    still don't work ;-)

--- AuxiliaireTest.php
+++ AuxiliaireTest.php
@@ -5,7 +5,7 @@ class AuxiliaireTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider regularAuxiliaireProvider
 	 */
 	public function testAuxiliaire($expectedResult, $verb) {
-		$this->assertEquals ( $expectedResult, $auxiliaire( $verb ) );
+		$this->assertEquals ( $expectedResult, auxiliaire( $verb ) );
 	}
 	public function regularAuxiliaireProvider() {
 		return array (
