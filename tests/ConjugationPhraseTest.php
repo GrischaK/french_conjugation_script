6526cfb9cb15d8930340d2caaacd439a31b6a706
GrischaK <grischa.kosba@gmail.com>
2015-06-19 13:23:16 +0200

    something is wrong

---
+++ tests/ConjugationPhraseTest.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'conjugate.php';
+
+class ConjugatePhraseTest extends PHPUnit_Framework_TestCase {
+	
+
+	/**
+	 * @dataProvider ConjugatePhraseTest
+	 */                                          
+	public function testConjugatePhraseTest($expectedResult, $verb, $tense, $person, $mood) {
+		$this->assertEquals ( $expectedResult, conjugation_phrase ( $verb, new Person($person), new Tense($tense), new Mood($mood) ) );
+	}						
+	public function ConjugatePhraseTest() {
+		;
+		return array (
+				array (
+						'Je aime',
+						'aimer',						
+						'Present',
+						'FirstPersonSingular',
+						'Indicatif' 
+				),
+				array (
+						'Je donne',
+						'donner',
+						'Present',
+						'ThirdPersonPlural',
+						'Indicatif' 
+				) 
+		);		
+	}
+}
+?>
