f869af1760066d4f9f04517049a3d30d5b90deff
GrischaK <grischa.kosba@gmail.com>
2015-06-19 13:23:42 +0200

    corrected expected value

--- tests/ConjugateTest.php
+++ tests/ConjugateTest.php
@@ -14,6 +14,7 @@ class ConjugateFrenchVerbTest extends PHPUnit_Framework_TestCase {
 		;
 		return array (
 				array (
+						'aime',						
 						'aimer',
 						'Present',
 						'FirstPersonSingular',
