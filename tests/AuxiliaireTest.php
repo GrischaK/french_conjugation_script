6f04aa4ab813f02ec4d0e6a62b00411e56a0c7f6
GrischaK <grischa.kosba@gmail.com>
2015-06-19 13:09:24 +0200

    corrected function

--- tests/AuxiliaireTest.php
+++ tests/AuxiliaireTest.php
@@ -7,7 +7,7 @@ class AuxiliaireTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider regularAuxiliaireProvider
 	 */
 	public function testAuxiliaire($expectedResult, $verb) {
-	$this->assertEquals ( new Auxiliaire($expectedResult), finding_auxiliaire (new Auxiliaire($verb)));
+	$this->assertEquals ( new Auxiliaire($expectedResult), finding_auxiliaire ($verb));
 	}                                                       
 	public function regularAuxiliaireProvider() {
 		return array (
