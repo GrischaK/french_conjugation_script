6c8e8c6cb19e2d613ab39258aa9a45acf5fc6a72
Nobody <ich@sektor.selfip.com>
2015-07-03 11:55:00 +0200

    Allowing for string values in the enum constants
    If there is a constant name and a value with equal strings, the constant
    is preferred

--- Enum.php
+++ Enum.php
@@ -1,20 +1,24 @@
 <?php
 abstract class Enum {
 	private $value;
+	
+	/**
+	 * @param $value Name or value of the constant to represent
+	 */
 	public function __construct($value) {
 		$constants = $this->getConstants ();
 		if (is_string ( $value )) {
 			if (isset ( $constants [$value] )) {
 				$this->value = $constants [$value];
-			} else {
+				return;
+			} else if(!Enum::containsStringValues($constants)) {
 				throw new Exception ( "No constant of name $value in Enum " . get_called_class () );
 			}
+		}
+		if (in_array ( $value, $constants )) {
+			$this->value = $value;
 		} else {
-			if (in_array ( $value, $constants )) {
-				$this->value = $value;
-			} else {
-				throw new Exception ( "No constant of value $value in Enum " . get_called_class () );
-			}
+			throw new Exception ( "No constant of value $value in Enum " . get_called_class () );
 		}
 	}
 
@@ -25,6 +29,14 @@ abstract class Enum {
 	static public function getConstants() {
 		$reflection = new ReflectionClass(get_called_class());
 		return $reflection->getConstants();
+	}
+	
+	static private function containsStringValues($array) {
+		foreach($array as $value) {
+			if(is_string($value))
+				return true;
+		}
+		return false;
 	}
 
 	public function __toString() {
