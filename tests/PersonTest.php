b1cceb44b3c1966d75e0a0723efeae9526bad5db
GrischaK <grischa.kosba@gmail.com>
2015-07-07 11:21:56 +0200

    added test results for  Subjonctif snd add modd to all test results, add mood as parameter

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -4,35 +4,72 @@ class PersonTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider PersonTestProvider
 	 */
-	public function testPerson($expectedResult, $person) {
+	public function testPerson($expectedResult, $person, $mood) {
 		$this->assertEquals ( $expectedResult, personal_pronoun(new Person($person)));
 	}
 	public function PersonTestProvider() {
 		return array (
 				array (
 						'je ',
+						'Indicatif',
 					    'FirstPersonSingular',
 				),
 				array (
 						'tu ',
+						'Indicatif',
 						'SecondPersonSingular',
 				),
 				array (
 						'il ',
+						'Indicatif',
 						'ThirdPersonSingular',
 				),
 				array (
 						'nous ',
+						'Indicatif',
 						'FirstPersonPlural',
 				),
 				array (
 						'vous ',
+						'Indicatif',
 						'SecondPersonPlural',						
-				),												
+				),						
 				array (
-						'ils ',
+						"qu'ils ",
+						'Indicatif',
+						'SecondPersonPlural',
+				),
+				array (
+						'que je ',
+						'Subjonctif',
+						'FirstPersonSingular',
+				),
+				array (
+						'que tu ',
+						'Subjonctif',
+						'SecondPersonSingular',
+				),
+				array (
+						"qu'il ",
+						'Subjonctif',
+						'ThirdPersonSingular',
+				),
+				array (
+						'que nous ',
+						'Subjonctif',
+						'FirstPersonPlural',
+				),
+				array (
+						'que vous ',
+						'Subjonctif',
+						'SecondPersonPlural',
+				),
+				array (
+						"qu'ils",
+						'Subjonctif',
 						'ThirdPersonPlural',
-				)				
+				)																					
+				
 		);
 	}
 }
