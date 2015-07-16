ad5c1367b8e10c7ec95ff4f841434f6067fb9d62
GrischaK <grischa.kosba@gmail.com>
2015-07-16 12:31:14 +0200

    corrected require file

--- tests/PrintTest.php
+++ tests/PrintTest.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'conjugate.php';
-
+require_once 'print.php';
 class PrintTest extends PHPUnit_Framework_TestCase {
 
 
@@ -8,8 +8,8 @@ class PrintTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider PrintProvider
 	 */
 	public function testPrint($expectedResult, $verb, $tense, $person, $mood) {
-		$this->assertEquals ( $expectedResult, conjugation_phrase ( $verb, new Person($person), new Tense($tense), new Mood($mood) ) );
-	}
+		$this->assertEquals ( $expectedResult, print_conjugations_of_verb ( $verb ) );
+	}                                                               
 	public function PrintProvider() {
 		;
 		return array (
