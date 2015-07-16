c5fc70db0cc32915f044b95f181b6863ec176af8
GrischaK <grischa.kosba@gmail.com>
2015-07-16 15:13:15 +0200

    corrected tenses array

--- print.php
+++ print.php
@@ -11,8 +11,32 @@ function print_persons($verb, Tense $tense, Mood $mood) {
 }
 
 function print_tenses($verb, Mood $mood) {
-	$tenses = array(Tense::Present,Tense::Imparfait,Tense::Passe,Tense::Futur, Tense::Futur_compose,Tense::Plus_que_parfait,
-			Tense::Passe_anterieur, Tense::Futur_anterieur,Tense::Premiere_Forme,Tense::Deuxieme_Forme);
+	$tenses = array (
+			Mood::Indicatif => array (
+					Tense::Present,
+					Tense::Imparfait,
+					Tense::Passe,
+					Tense::Futur,
+					Tense::Passe_compose,
+					Tense::Plus_que_parfait,
+					Tense::Passe_anterieur 
+			),
+			Mood::Subjonctif => array (
+					Tense::Present,
+					Tense::Imparfait,
+					Tense::Passe,
+					Tense::Plus_que_parfait 
+			),
+			Mood::Conditionnel => array (
+					Tense::Present,
+					Tense::Premiere_Forme,
+					Tense::Deuxieme_Forme 
+			),
+			Mood::Imperatif => array (
+					Tense::Present,
+					Tense::Passe 
+			) 
+	);
 	foreach ( $tenses as $tense ) {
 		print_persons ( $verb, new Tense($tense),new Mood($mood) ); 
 	}	
