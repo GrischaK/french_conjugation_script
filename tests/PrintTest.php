07492d0ed53e5a3c42d33b647d3213526f9d69c6
GrischaK <grischa.kosba@gmail.com>
2015-07-16 12:26:26 +0200

    new test for printing

---
+++ tests/PrintTest.php
@@ -0,0 +1,110 @@
+<?php
+require_once 'conjugate.php';
+
+class PrintTest extends PHPUnit_Framework_TestCase {
+
+
+	/**
+	 * @dataProvider PrintProvider
+	 */
+	public function testPrint($expectedResult, $verb, $tense, $person, $mood) {
+		$this->assertEquals ( $expectedResult, conjugation_phrase ( $verb, new Person($person), new Tense($tense), new Mood($mood) ) );
+	}
+	public function PrintProvider() {
+		;
+		return array (
+				array (
+						'j’ai aimé',
+						'aimer',
+						'Passe_compose',
+						'FirstPersonSingular',
+						'Indicatif'
+				),	
+				array (
+						'je suis amusé',
+						'amuser',
+						'Passe_compose',
+						'FirstPersonSingular',
+						'Indicatif'
+				),
+				array (
+						'ils sont amusés',
+						'amuser',
+						'Passe_compose',
+						'ThirdPersonPlural',
+						'Indicatif'
+				),											
+				array (
+						'j’aime',
+						'aimer',	
+						'Present',						
+						'FirstPersonSingular',											
+						'Indicatif' 
+				),	
+				array (
+						'que j’aime',
+						'aimer',
+						'Present',
+						'FirstPersonSingular',
+						'Subjonctif'
+				),		
+				array ( 
+						'aime',
+						'aimer',
+						'Present',
+						'FirstPersonSingular',
+						'Imperatif'
+				),
+				array (
+						'aimons',
+						'aimer',
+						'Present',
+						'FirstPersonPlural',
+						'Imperatif'
+				),
+				array (
+						'aimez',
+						'aimer',
+						'Present',
+						'SecondPersonPlural',
+						'Imperatif'
+				),	
+				array (
+						'aie aimé',
+						'aimer',
+						'Passe',
+						'FirstPersonSingular',
+						'Imperatif'
+				),
+				array ( 
+						'ayons aimé',
+						'aimer',
+						'Passe',
+						'FirstPersonPlural',
+						'Imperatif'
+				),
+				array ( 
+						'ayez aimé',
+						'aimer',
+						'Passe',
+						'SecondPersonPlural',
+						'Imperatif'
+				),											
+				array (
+						'je donne',
+						'donner',
+						'Present',
+						'FirstPersonSingular',
+						'Indicatif' 
+				), 
+				array (
+						'je vais donner',
+						'donner',
+						'Futur_compose',
+						'FirstPersonSingular',
+						'Indicatif'
+				)
+		);
+	}
+}
+?>
