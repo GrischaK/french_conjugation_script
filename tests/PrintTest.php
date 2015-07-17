e7c988f85479839dd7aa0acf5913c438de111de0
GrischaK <grischa.kosba@gmail.com>
2015-07-17 12:35:06 +0200

    delete trailing spaces

--- tests/PrintTest.php
+++ tests/PrintTest.php
@@ -13,13 +13,13 @@ class PrintTest extends PHPUnit_Framework_TestCase {
 	public function PrintProvider() {
 		return array (
 				array (
-'j’aime 
-tu aimes 
-il aime 
-nous aimons 
+'j’aime
+tu aimes
+il aime
+nous aimons
 vous aimez
 ils aiment
-						
+					
 j’aimais
 tu aimais
 il aimait
@@ -31,7 +31,7 @@ j’aimai
 tu aimas
 il aima
 nous aimâmes
-vous aimâtes 						
+vous aimâtes						
 ils aimèrent
 						 		
 j’aimerai
@@ -47,39 +47,39 @@ il a aimé
 nous avons aimé
 vous avez aimé
 ils ont aimé
-										
+								
 j’avais aimé
 tu avais aimé
 il avait aimé
 nous avions aimé
 vous aviez aimé
 ils avaient aimé
-						
+					
 j’eus aimé
 tu eus aimé
 il eut aimé
 nous eûmes aimé
 vous eûtes aimé
 ils eurent aimé
-							       
+				       
 j’aurai aimé
 tu auras aimé
 il aura aimé
 nous aurons aimé
 vous aurez aimé
 ils auront aimé
-					       
+	       
 je vais aimer
 tu vas aimer
 il va aimer
 nous allons aimer 
 vous allez aimer
-ils vont aimer 	
-			
-que j’aime 
+ils vont aimer	
+
+que j’aime
 que tu aimes 
-qu’il aime 
-que nous aimons 
+qu’il aime
+que nous aimons
 que vous aimez
 qu’ils aiment
 
@@ -88,12 +88,12 @@ que tu aimais
 qu’il aimait
 que nous aimions
 que vous aimiez
-qu’ils aimaient	
+qu’ils aimaient
 
 que j’aie aimé
-que tu aies aimé 
-qu’il ait aimé 
-que nous ayons aimé 
+que tu aies aimé
+qu’il ait aimé
+que nous ayons aimé
 que vous ayez aimé
 qu’ils aient aimé
 
@@ -102,8 +102,8 @@ que tu eusses aimé
 qu’il eût aimé
 que nous eussions aimé
 que vous eussiez aimé
-qu’ils eussent aimé	
-						
+qu’ils eussent aimé
+			
 j’aimerais
 tu aimerais
 il aimerait
@@ -124,15 +124,14 @@ il eût aimé
 nous eussions aimé
 vous eussiez aimé
 ils eussent aimé
-						
+			
 aime
 aimons
 aimez
 
 aie aimé
 ayons aimé
-ayez aimé									
-',
+ayez aimé',
 // mode impersonnels						
 //aimer
 //avoir aimé
