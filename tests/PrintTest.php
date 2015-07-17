ae8f1fd73e13d6d7e53a4da4010ba4d12e74e2ae
GrischaK <grischa.kosba@gmail.com>
2015-07-17 11:09:31 +0200

    test results for all forms in mood indicatif

--- tests/PrintTest.php
+++ tests/PrintTest.php
@@ -13,7 +13,71 @@ class PrintTest extends PHPUnit_Framework_TestCase {
 	public function PrintProvider() {
 		return array (
 				array (
-						'',
+'
+						j’aime 
+						tu aimes 
+						il aime 
+						nous aimons 
+						vous aimez
+						ils aiment
+						
+						j’aimais
+						tu aimais
+						il aimait
+						nous aimions
+						vous aimiez
+						ils aimaient	
+
+    					j’aimai
+						tu aimas
+						il aima
+						nous aimâmes
+						vous aimâtes 						
+						ils aimèrent
+						 		
+    					j’aimerai
+						tu aimeras
+						il aimera
+						nous aimerons
+						vous aimerez
+						ils aimeront
+
+    	                j’ai aimé
+						tu as aimé
+						il a aimé
+						nous avons aimé
+						vous avez aimé
+						ils ont aimé
+										
+    	                j’avais aimé
+						tu avais aimé
+						il avait aimé
+						nous avions aimé
+						vous aviez aimé
+						ils avaient aimé
+						
+    					j’eus aimé
+						tu eus aimé
+						il eut aimé
+						nous eûmes aimé
+						vous eûtes aimé
+						ils eurent aimé
+							       
+    	                j’aurai aimé
+						tu auras aimé
+						il aura aimé
+						nous aurons aimé
+						vous aurez aimé
+						ils auront aimé
+					       
+    		            je vais aimer
+    	                tu vas aimer
+    	                il va aimer
+    	                nous allons aimer 
+    	                vous allez aimer
+    	                ils vont aimer 						
+',
+
 						'aimer' 
 				) 
 		);
