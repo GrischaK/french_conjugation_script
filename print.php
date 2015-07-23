1fc855adcdb8fd985604f1754c5ba7d260382a2e
GrischaK <grischa.kosba@gmail.com>
2015-07-23 19:29:07 +0200

    now without calling twice the functio

--- print.php
+++ print.php
@@ -150,8 +150,9 @@ function print_persons($verb, Tense $tense, Mood $mood) {
 			) 
 	);
 	foreach ( $persons [$mood->getValue ()] [$tense->getValue ()] as $person ) {
-		echo '<tr><td><span data-text="' . conjugation_phrase ( $verb, new Person ( $person ), ($tense), ($mood) ) . '" data-lang="fr" class="trigger_play"></span></td>';
-		echo '<td>' . conjugation_phrase ( $verb, new Person ( $person ), ($tense), ($mood) ) . '</td></tr>' . PHP_EOL;
+		$output = conjugation_phrase ( $verb, new Person ( $person ), $tense, $mood);
+		echo '<tr><td><span data-text="' . $output . '" data-lang="fr" class="trigger_play"></span></td>';
+		echo '<td>' . $output . '</td></tr>' . PHP_EOL;
 	}   
 }
 
