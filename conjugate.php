a753d9d4f935cc9e164ec8628054b27be705a9c0
GrischaK <grischa.kosba@gmail.com>
2015-07-24 15:28:42 +0200

    change mistakes

--- conjugate.php
+++ conjugate.php
@@ -511,10 +511,10 @@ class GoogleTTSConjugationPhraseVisitor extends ConjugationPhraseVisitor {
 	function visitCompositeTense(CompositeTenseConjugationPhrase $visitee) {
 		return concatenate_apostrophized ( $visitee->personal_pronoun, $visitee->conjugated_auxiliaire_verb). $visitee->participe_passe ;
 	}
-	function visitImperatifPresentTense(ImperatifTensePresentConjugationPhrase $visitee) {
+	function visitImperatifPresentTense(ImperatifPresentTenseConjugationPhrase $visitee) {
 		return $visitee->conjugated_verb;
 	}
-	function visitImperatifPasseTense(ImperatifTensePasseConjugationPhrase $visitee) {
+	function visitImperatifPasseTense(ImperatifPasseTenseConjugationPhrase $visitee) {
 		return $visitee->conjugated_auxiliaire_verb. $visitee->participe_passe;
 	}	
 	function visitFuturComposeTense(FuturComposeTenseConjugationPhrase $visitee) {
