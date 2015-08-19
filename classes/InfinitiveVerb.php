2f93a6cd7039a8fa8c9a0c90c634c26552c8b73a
GrischaK <grischa.kosba@gmail.com>
2015-08-19 14:20:53 +0200

    new class

---
+++ classes/InfinitiveVerb.php
@@ -0,0 +1,11 @@
+<?php
+class InfinitiveVerb {
+	private $infinitivverb;
+	
+	public function __construct($infinitivverb) {
+		if(is_a($infinitivverb, get_called_class ())) {
+			$this->value = $value->value;
+			return;
+		}	
+}
+?>
