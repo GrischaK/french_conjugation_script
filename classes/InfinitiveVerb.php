1a27ba6b07383161997845ea73e37b6403c9e4ff
Nobody <ich@sektor.selfip.com>
2015-08-20 18:38:32 +0200

    Only allow construction from InfinitiveVerb and string

--- classes/InfinitiveVerb.php
+++ classes/InfinitiveVerb.php
@@ -2,7 +2,12 @@
 class InfinitiveVerb {
 	private $infinitiveVerb;
 	public function __construct($infinitiveVerb) {
-		$this->infinitiveVerb = $infinitiveVerb;
+		if($infinitiveVerb instanceof InfinitiveVerb) {
+			$this->infinitiveVerb = $infinitiveVerb->getInfinitiveVerb();
+		} else {
+			assert(is_string($infinitiveVerb));
+			$this->infinitiveVerb = $infinitiveVerb;
+		}
 	}
 
    public function getInfinitiveVerb() {
