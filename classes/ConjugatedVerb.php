569d9fa8571aab868c75c0abe41c1d9bae733f79
GrischaK <grischa.kosba@gmail.com>
2015-08-21 11:06:28 +0200

    new class ConjugatedVerb.php

---
+++ classes/ConjugatedVerb.php
@@ -0,0 +1,21 @@
+<?php
+class ConjugatedVerb {
+	private $conjugatedVerb;
+	public function __construct($conjugatedVerb) {
+		if($conjugatedVerb instanceof $conjugatedVerb) {
+			$this->conjugatedVerb = $conjugatedVerb->getConjugatedVerb();
+		} else {
+			assert(is_string($conjugatedVerb));
+			$this->conjugatedVerb = $conjugatedVerb;
+		}
+	}
+
+   public function getConjugatedVerb() {
+	return $this->conjugatedVerb;
+   }
+   
+   public function __toString() {
+   	return $this->conjugatedVerb;
+   }
+}
+?>
