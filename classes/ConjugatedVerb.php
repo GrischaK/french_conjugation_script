3fe53f37c6ca9326a85108c4953a3e38f2912f56
GrischaK <grischa.kosba@gmail.com>
2015-08-21 12:43:35 +0200

    corrected

--- classes/ConjugatedVerb.php
+++ classes/ConjugatedVerb.php
@@ -1,18 +1,23 @@
 <?php
 class ConjugatedVerb {
-	private $conjugatedVerb, $verb, $mode, $tense, $person;
+	private $verb, $mode, $tense, $person;	
 	public function __construct(InfinitiveVerb $infinitiveVerb, Mode $mode, Tense $tense, Person $person) {
 		$this->mode = $mode;
 		$this->tense = $tense;
 		$this->person = $person;
 	}
-
+	private $conjugatedVerb;
+	public function __construct($conjugatedVerb) {
+		if($conjugatedVerb instanceof ConjugatedVerb) {
+			$this->conjugatedVerb = $conjugatedVerb->getConjugatedVerb();
+		}	
+	}
 public function getConjugatedVerb() {
-	return $this->$conjugatedVerb;
+	return $this->conjugatedVerb;
 }
  
 public function __toString() {
-	return $this->$conjugatedVerb;
+	return $this->conjugatedVerb;
 }
 
 }
