d5f5ec2bb375b69b5d2f13bd46cb4b61b1db90a8
GrischaK <grischa.kosba@gmail.com>
2015-08-25 11:15:04 +0200

    add protected function initialize

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,12 +1,15 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
-	private $conjugatedAuxiliaireVerb, $verb, $mode, $tense, $person;
+	private $conjugatedAuxiliaireVerb, $auxiliaire, $verb, $mode, $tense, $person;
 	public function __construct(Auxiliaire $auxiliaire, Mode $mode, Tense $tense, Person $person) {
-		$this->auxiliaire = $auxiliaire; // ???
+		initialize($conjugatedVerb, $auxiliaire,  $mode,  $tense,  $person);
+	} 
+	protected function initialize($conjugatedVerb, $auxiliaire,  $mode,  $tense,  $person){
+		$this->auxiliaire = $auxiliaire;
 		$this->mode = $mode;
 		$this->tense = $tense;
-		$this->person = $person;
-	} 
+		$this->person = $person;		
+	} 	
 	public function __toString() {
 		return $this->conjugatedAuxiliaireVerb;
 	}
