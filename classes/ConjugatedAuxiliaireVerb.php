0f05e9675b54d84fe8f9d765efa67b570bd28f4a
GrischaK <grischa.kosba@gmail.com>
2015-08-25 11:21:17 +0200

    using initialize function in both classes

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -2,14 +2,9 @@
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
 	private $conjugatedAuxiliaireVerb, $auxiliaire, $verb, $mode, $tense, $person;
 	public function __construct(Auxiliaire $auxiliaire, Mode $mode, Tense $tense, Person $person) {
-		initialize($conjugatedVerb, $auxiliaire,  $mode,  $tense,  $person);
-	} 
-	protected function initialize($conjugatedVerb, $auxiliaire,  $mode,  $tense,  $person){
 		$this->auxiliaire = $auxiliaire;
-		$this->mode = $mode;
-		$this->tense = $tense;
-		$this->person = $person;		
-	} 	
+		initialize($conjugatedVerb,  $mode,  $tense,  $person);
+	} 
 	public function __toString() {
 		return $this->conjugatedAuxiliaireVerb;
 	}
--- classes/ConjugatedVerb.php
+++ classes/ConjugatedVerb.php
@@ -2,10 +2,14 @@
 class ConjugatedVerb {
 	private $conjugatedVerb, $verb, $mode, $tense, $person;
 	public function __construct(InfinitiveVerb $infinitiveVerb, Mode $mode, Tense $tense, Person $person) {
+		initialize($conjugatedVerb,  $mode,  $tense,  $person);
+	}
+	protected function initialize($conjugatedVerb,  $mode,  $tense,  $person){
+	
 		$this->mode = $mode;
 		$this->tense = $tense;
 		$this->person = $person;
-	}
+	}	
 	public function __toString() {
 		return $this->conjugatedVerb;
 	}
