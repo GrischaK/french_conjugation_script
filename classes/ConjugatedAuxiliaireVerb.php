7308ec059720b3e966075776618e068067881d68
Nobody <ich@sektor.selfip.com>
2015-08-27 11:59:13 +0200

    Storing the infinitive form for a conjugated verb as well

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,8 +1,9 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
 	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood) {
+		$infinitiveVerb = new InfinitiveVerb($auxiliaire->getInfinitiveString());
 		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood);
-		$this->initialize ( $conjugatedVerb, $mood, $tense, $person );
+		$this->initialize ( $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person );
 	}
 }
 ?>
--- classes/ConjugatedVerb.php
+++ classes/ConjugatedVerb.php
@@ -3,14 +3,20 @@ class ConjugatedVerb {
 	private $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person;
 	public function __construct(InfinitiveVerb $infinitiveVerb, Person $person, Tense $tense, Mood $mood) {
 		$conjugatedVerb = conjugate ( $infinitiveVerb, $person, $tense, $mood );
-		$this->initialize ( $conjugatedVerb, $mood, $tense, $person );
+		$this->initialize ( $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person );
 	}
-	protected function initialize($conjugatedVerb, $mood, $tense, $person) {
+	protected function initialize($conjugatedVerb, InfinitiveVerb $infinitiveVerb, Mood $mood, Tense $tense, Person $person) {
 		$this->mood = $mood;
 		$this->tense = $tense;
 		$this->person = $person;
 		$this->conjugatedVerb = $conjugatedVerb;
+		$this->infinitiveVerb = $infinitiveVerb;
 	}
+	
+	public function getInfinitive() {
+		return $this->infinitiveVerb;
+	}
+	
 	public function __toString() {
 		return $this->conjugatedVerb;
 	}
