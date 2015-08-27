5ba5ba72e8efd2ddfbfbd8c52d172888d7fa2364
GrischaK <grischa.kosba@gmail.com>
2015-08-27 11:10:11 +0200

    some corrections

--- classes/ConjugatedVerb.php
+++ classes/ConjugatedVerb.php
@@ -1,12 +1,12 @@
 <?php
 class ConjugatedVerb {
-	private $conjugatedVerb, $infinitiveVerb, $mode, $tense, $person;
-	public function __construct(InfinitiveVerb $infinitiveVerb, Mode $mode, Tense $tense, Person $person) {
+	private $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person;
+	public function __construct(InfinitiveVerb $infinitiveVerb, Mood $mood, Tense $tense, Person $person) {
 		$conjugatedVerb = conjugate($infinitiveVerb->getInfinitiveVerb(),  $person->getperson(),  $tense->gettense(),  $mood->getmood());
-		initialize($conjugatedVerb,  $mode,  $tense,  $person);
+		initialize($conjugatedVerb,  $mood,  $tense,  $person);
 	}
-	protected function initialize($conjugatedVerb,  $mode,  $tense,  $person){	
-		$this->mode = $mode;
+	protected function initialize($conjugatedVerb,  $mood,  $tense,  $person){	
+		$this->mood = $mood;
 		$this->tense = $tense;
 		$this->person = $person;
 	}	
--- conjugate.php
+++ conjugate.php
@@ -587,7 +587,7 @@ abstract class ConjugationPhrase {
 			}
 			return new CompositeTenseConjugationPhrase ( $personal_pronoun, $conjugated_auxiliaire_verb, $participe_passe ); 
 		} else {
-			$conjugated_verb = new ConjugatedVerb($infinitiveVerb, $mode, $tense, $person);
+			$conjugated_verb = new ConjugatedVerb($infinitiveVerb, $mood, $tense, $person);
 			if ($mood->getValue () === Mood::Imperatif) {
 				return new ImperatifPresentTenseConjugationPhrase ( $conjugated_verb );
 			}
