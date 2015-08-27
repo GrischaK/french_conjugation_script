e062bb459d954f1beeb74413acc2f44b359588e3
Nobody <ich@sektor.selfip.com>
2015-08-27 11:38:55 +0200

    Calculated conjugated auxiliaire verb

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,6 +1,7 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
 	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood) {
+		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood);
 		$this->initialize ( $conjugatedVerb, $mood, $tense, $person );
 	}
 	public function __toString() {
