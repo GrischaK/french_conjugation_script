8072c5b93e2c6694c0c76004b141bb75023b545f
GrischaK <grischa.kosba@gmail.com>
2015-08-27 11:24:59 +0200

    add Auxiliaire $auxiliaire

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,8 +1,7 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
-	private $conjugatedAuxiliaireVerb, $auxiliaire, $infinitiveVerb, $mood, $tense, $person;
-	public function __construct(Mood $mood, Tense $tense, Person $person) {
-		initialize ( $conjugatedVerb, $mood, $tense, $person );
+	public function __construct(Auxiliaire $auxiliaire, Mood $mood, Tense $tense, Person $person) {
+		$this->initialize ( $conjugatedVerb, $mood, $tense, $person );
 	}
 	public function __toString() {
 		return $this->conjugatedAuxiliaireVerb;
