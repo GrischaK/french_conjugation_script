d6c430f59050f77a70bf531e36050ceb9be9bc02
GrischaK <grischa.kosba@gmail.com>
2015-08-27 11:13:08 +0200

    corrected one line

--- classes/ConjugatedVerb.php
+++ classes/ConjugatedVerb.php
@@ -2,14 +2,14 @@
 class ConjugatedVerb {
 	private $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person;
 	public function __construct(InfinitiveVerb $infinitiveVerb, Mood $mood, Tense $tense, Person $person) {
-		$conjugatedVerb = conjugate($infinitiveVerb->getInfinitiveVerb(),  $person->getperson(),  $tense->gettense(),  $mood->getmood());
-		initialize($conjugatedVerb,  $mood,  $tense,  $person);
+		$conjugatedVerb = conjugate ( $infinitiveVerb->getInfinitiveVerb (), $person, $tense, $mood );
+		initialize ( $conjugatedVerb, $mood, $tense, $person );
 	}
-	protected function initialize($conjugatedVerb,  $mood,  $tense,  $person){	
+	protected function initialize($conjugatedVerb, $mood, $tense, $person) {
 		$this->mood = $mood;
 		$this->tense = $tense;
 		$this->person = $person;
-	}	
+	}
 	public function __toString() {
 		return $this->conjugatedVerb;
 	}
