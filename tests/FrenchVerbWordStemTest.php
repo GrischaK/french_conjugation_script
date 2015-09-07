34224b57b1f14b5be29069f287ae2eef215bf2ad
GrischaK <grischa.kosba@gmail.com>
2015-09-07 12:11:11 +0200

    corrected parameter

--- tests/FrenchVerbWordStemTest.php
+++ tests/FrenchVerbWordStemTest.php
@@ -4,14 +4,17 @@ class FrenchVerbWordStemTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider regularWordStemProvider
 	 */
-	public function testWordStem($expectedResult, $infinitiveVerb) {
-		$this->assertEquals ( $expectedResult, word_stem ( new InfinitiveVerb($infinitiveVerb) ) );
+	public function testWordStem($expectedResult, $infinitiveVerb, $person, $tense, $mood) {
+		$this->assertEquals ( $expectedResult, word_stem ( new InfinitiveVerb( $infinitiveVerb), new Person($person), new Tense($tense), new Mood($mood) ) );
 	}
 	public function regularWordStemProvider() {
 		return array (
 				array (
 						'aim',
-						'aimer'
+						'aimer',
+						'FirstPersonSingular',
+						'Present',
+						'Indicatif'
 				)
 		);
 	}
