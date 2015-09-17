0bacf55e18b386ad8c129f0e60615787c5bb3acf
GrischaK <grischa.kosba@gmail.com>
2015-09-17 19:39:13 +0200

    $ohne_iss = array_merge(mouvoir);

--- classes/IrregularExceptionGroup.php
+++ classes/IrregularExceptionGroup.php
@@ -2,5 +2,9 @@
 class IrregularExceptionGroup {
 	public static $vetir = array('dévêtir','redévêtir','revêtir','se dévêtir','se redévêtir','se sous-vêtir','se vêtir','sous-vêtir','survêtir','vêtir');
 	public static $mouvoir = array('autopromouvoir','auto-promouvoir','émouvoir','mouvoir','promouvoir','réémouvoir','remouvoir','repromouvoir','se mouvoir'); // 9 Verben
+
+	public static function initVars() {
+		$ohne_iss = array_merge(mouvoir);
+	}	
 }
 ?>      
