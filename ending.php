1225eca54b0cb4519ae058fabd8663a5cfb4ff67
GrischaK <grischa.kosba@gmail.com>
2015-09-24 14:47:54 +0200

    old ending_ir renamend

--- ending.php
+++ ending.php
@@ -94,6 +94,13 @@ function ending_er(Person $person, Tense $tense, Mood $mood, EndingWith $endingw
 	return $ending [$endingwith->getValue ()][$mood->getValue ()] [$tense->getValue ()] [$person->getValue ()];
 }
 function ending_ir(Person $person, Tense $tense, Mood $mood, EndingWith $endingwith, ExceptionModel $exceptionModel) {
+	if() {
+		return ending_ir_without_iss(Person $person, Tense $tense, Mood $mood, EndingWith $endingwith, ExceptionModel $exceptionModel);
+	} else {
+		return ending_ir_with_iss(Person $person, Tense $tense, Mood $mood, EndingWith $endingwith, ExceptionModel $exceptionModel);
+	}	
+}
+function ending_ir_with_iss(Person $person, Tense $tense, Mood $mood, EndingWith $endingwith, ExceptionModel $exceptionModel) {
 	$ending = array (
 			EndingWith::IR => array ( // standard endings for verbs ending with -ir
 					Mood::Indicatif => array (
