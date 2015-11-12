ebaff1b5c80e3d22570293b91c1aa0ad5b793adb
Nobody <ich@sektor.selfip.com>
2015-11-12 11:43:42 +0100

    Corrected getVerbsThatUse

--- classes/Auxiliaire.php
+++ classes/Auxiliaire.php
@@ -9,20 +9,18 @@ class Auxiliaire extends Enum{
 	}
 	
 	static function getVerbsThatUse(Auxiliaire $auxiliaire) {
-		$aux_etre = array ('accourir','advenir','aller','amuser','apparaitre','apparaître','arriver','ascendre','co-naitre','co-naître','convenir',
+		$verbs = array(Auxiliaire::Etre => array ('accourir','advenir','aller','amuser','apparaitre','apparaître','arriver','ascendre','co-naitre','co-naître','convenir',
 			'débeller','décéder','démourir','descendre','disconvenir','devenir','échoir','entre-venir','entrer','époustoufler','intervenir',
 			'issir','mévenir','monter','mourir','naitre','naître','obvenir','paraitre','paraître','partir','parvenir','pourrir','prémourir',
 			'provenir','ragaillardir','raller','réadvenir','re-aller','réapparaitre','réapparaître','reconvenir','redépartir','redescendre',
 			'redevenir','réentrer','réintervenir','remonter','remourir','renaitre','renaître','rentrer','revenir','reparaitre','reparaître',
 			'repartir','reparvenir','repasser','repourrir','rerentrer','rerester','ressortir','ressouvenir','rester','resurvenir','retomber',
 			'retourner','retrépasser','revenir','s’amuser','se redépartir','se sortir','se souvenir','sortir','souvenir','stationner','sur-aller'
-			,'suradvenir','survenir','tomber','trépasser','venir'); 	
-	
-		$aux_avoir_etre = array ('accourir','ascendre','convenir','déchoir','demeurer','descendre','disparaitre','disparaître','disconvenir',
+			,'suradvenir','survenir','tomber','trépasser','venir'), 	
+		Auxiliaire::AvoirandEtre => array ('accourir','ascendre','convenir','déchoir','demeurer','descendre','disparaitre','disparaître','disconvenir',
 			'éclore','enclore','entrer ','monter','paraitre','paraître','passer','ragaillardir', 'ré-apparaître','réapparaître','reconvenir',
-			'reparaitre','reparaître','sortir','tomber'); // use both auxiliary verbs	
-	return $aux_etre;
-			
+			'reparaitre','reparaître','sortir','tomber'));	
+		return $verbs[$auxiliaire->getValue()];
 	}
 }
 ?>
