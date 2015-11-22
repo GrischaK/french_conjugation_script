8899eb8e6ea72bd2b6ac98f975d30f173b03b1a2
GrischaK <grischa.kosba@gmail.com>
2015-11-22 14:22:49 +0100

    add more ExceptionModel's

--- classes/ExceptionModel.php
+++ classes/ExceptionModel.php
@@ -2,12 +2,32 @@
 class ExceptionModel extends Enum {
 	const NO_EXCEPTIONS = 'no exceptions';
 	//const WITHOUT_ISS_ENDING = 'without iss-ending';
-	
-	const VETIR = 'vetir';  
-	
-// OIR	
-	const MOUVOIR = 'mouvoir';
 
+	const Eler_Ele = 'peler';
+	const Eter_Ete = 'acheter';
+
+	const Eler_Elle = 'appeler';
+	const Eter_Ette = 'jeter';
+	
+	const ENVOYER = 'envoyer';
+	const CER = 'cer';
+	const GER = 'ger';
+	
+	const E_Akut_Er = 'e_akut_er';
+	const E_Er = 'e_er';
+		
+// OIR	
+	const DEVOIR = 'devoir';
+	const MOUVOIR = 'mouvoir';
+	const POUVOIR = 'pouvoir';
+	const SAVOIR = 'savoir';
+	
+// RIR
+	const COURIR = 'courir';
+	const MOURIR = 'mourir';
+	const QUERIR = 'querir';
+	const VETIR = 'vetir';
+	
 	//const SEOIR = 'seoir';
 }
 ?>
