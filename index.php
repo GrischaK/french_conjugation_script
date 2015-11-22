84e4b4b09113a611cf9b92c01eed9889e35241d8
GrischaK <grischa.kosba@gmail.com>
2015-11-22 14:41:45 +0100

    no important changes

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <?php
-require_once 'translation.php';
+//require_once 'translation.php';
 require_once 'verbs.php';
 require_once 'conjugate.php';
 require_once 'print.php';
 include($_SERVER['DOCUMENT_ROOT']."/data/before_content.php");
+?>
+<style type="text/css">
+#menu,#rechts  {display:none;}
+#rechts.col-md-2{display:none;}	  
+#content.col-md-7 {width:85%;}
+ 
+@media 
+  (min-device-width: 800px) 
+  and (max-device-width: 1280px) {
+#menu.col-md-3 {width:23%;}
+#content.col-md-7 {width:77%;}
+}
+</style> 
+<?php
+$kategorien=array(
+"erste-gruppe","zweite-gruppe","dritte-gruppe",
+"hifsverb-avoir","hilfsverb-etre","hilfsverb-avoir-etre",
+"reflexiv","irreflexiv","ausschließlich-reflexiven-Verben","transitiv","intransitiv",'altfrannzösische-Sprache','defekte Verben','unpersönliche Verben',
 
-
-$kategorien=array("erste-gruppe","zweite-gruppe","dritte-gruppe",
-					"hifsverb-avoir","hilfsverb-etre","hilfsverb-avoir-etre",
-					"reflexiv","irreflexiv","ausschließlich-reflexiven-Verben","transitiv","intransitiv",'altfrannzösische-Sprache','defekte Verben','unpersönliche Verben',
-	"endung-cer","endung-ier","endung-ger","endung-eler_ele","endung-eler_elle","endung-eter_ete","endung-eter_ette","endung-yer_ie","endung-é_er","endung-écer","endung-éger","endung-éyer","endung-envoyer",
-	"endung-vouloir","endung-avoir_ravoir","endung-voir","endung-cevoir","endung-devoir",
-	"endung-mouvoir","endung-pleuvoir","endung-pouvoir","endung-savoir","endung-falloir",
-	"endung-seoir","endung-valoir","endung-haïr",
-	"endung-indre","endung-battre","endung-crire","endung-mettre","endung-prendre","endung-rompre","endung-être","endung-aire","endung-faire");
+"endung-cer","endung-ier","endung-ger","endung-eler_ele","endung-eler_elle","endung-eter_ete","endung-eter_ette","endung-yer_ie","endung-é_er","endung-écer","endung-éger","endung-éyer","endung-envoyer",
+"endung-vouloir","endung-avoir_ravoir","endung-voir","endung-cevoir","endung-devoir","endung-mouvoir","endung-pleuvoir","endung-pouvoir","endung-savoir","endung-falloir","endung-seoir","endung-valoir","endung-haïr",
+"endung-indre","endung-battre","endung-crire","endung-mettre","endung-prendre","endung-rompre","endung-être","endung-aire","endung-faire");
 $titles=array();
 for($a=0;$a<count($kategorien);$a++){
 	if($a<3){
