cef9c838f0a15139d8419a28ddbe3e3b4309f25f
GrischaK <grischa.kosba@gmail.com>
2015-11-22 13:56:25 +0100

    $verbs changed to $infinitiveVerb

--- text.php
+++ text.php
@@ -2,71 +2,60 @@
 /*
 Hier findet man alles, was über den Konjugationstabellen steht.
 */ 
-$regelmaessig = in_array($verb, $unregelmaessige_verben) ? 'unregelmäßig' : 'regelmäßig'; 
-$reflexiv = in_array($verb, $verbes_pronominaux) ? 'reflexives' : 'nicht reflexives';
+$regelmaessig = in_array($infinitiveVerb, $unregelmaessige_verben) ? 'unregelmäßig' : 'regelmäßig'; 
+$reflexiv = in_array($infinitiveVerb, $verbes_pronominaux) ? 'reflexives' : 'nicht reflexives';
 $conditions = array();
-if (in_array($verb, $verbes_transitifs)) {
+if (in_array($infinitiveVerb, $verbes_transitifs)) {
   $conditions[] = 'transitives';
 }
-if (in_array($verb, $verbes_intransitifs)) {
+if (in_array($infinitiveVerb, $verbes_intransitifs)) {
   $conditions[] = 'intransitives';
 }
-if (!in_array($verb, $verbes_transitifs) and !in_array($verb, $verbes_intransitifs)) {
+if (!in_array($infinitiveVerb, $verbes_transitifs) and !in_array($infinitiveVerb, $verbes_intransitifs)) {
   $conditions[] = '???';
 }
 $transitiv = implode(' oder ', $conditions);
-  
-  if ((substr ( $verb, - 2, 2 ) == 'er') &&  (!in_array($verb,array('aller','raller','re-aller','saisir-arrêter','saisir-brandonner','saisir-exécuter','s’en aller','sur-aller')))) {
+
+//$substr_er = array_filter($infinitiveVerb, function($value) { return substr($value, -2) == 'er'; });
+  if ((substr ( $infinitiveVerb, - 2, 2 ) == 'er') &&  (!in_array($infinitiveVerb,array('aller','raller','re-aller','saisir-arrêter','saisir-brandonner','saisir-exécuter','s’en aller','sur-aller')))) {
     $gruppe = 'der 1. Verbgruppe';
   } 
-  elseif ((substr ( $verb, - 2, 2 ) == 'ir') or (in_array(mb_substr("$verb", - 2, 2, "utf-8"), array("ïr"))) && !in_array($verb, $unregel_ir)) {
+  elseif ((substr ( $infinitiveVerb, - 2, 2 ) == 'ir') or (in_array(mb_substr($infinitiveVerb, - 2, 2, "utf-8"), array("ïr"))) && !in_array($infinitiveVerb, $unregel_ir)) {
     $gruppe = 'der 2. Verbgruppe';
   } 
-  elseif ($verb == 'saisir-brandonner') {
+  elseif ($infinitiveVerb == 'saisir-brandonner') {
     $gruppe = 'keiner Verbgruppe';
   }   
   else { // aller,....
     $gruppe = 'der 3. Verbgruppe';
   }
-  require_once 'verbs.php';
-  require_once 'groups/irregular.php';
-  
-  require_once 'groups/verbes_pronominaux.php';
-  require_once 'groups/verbes_exclusivement_pronominaux.php';
-  require_once 'groups/verbes_intransitifs.php';
-  require_once 'groups/verbes_transitifs.php';
-  require_once 'groups/irregular-verb-groups.php';
+require_once 'classes/InfinitiveVerb.php';    
+require_once 'verbs.php';
+require_once 'groups/verbes_pronominaux.php';
+require_once 'groups/verbes_exclusivement_pronominaux.php';
+require_once 'groups/verbes_intransitifs.php';
+require_once 'groups/verbes_transitifs.php';
+require_once 'groups/irregular-verb-groups.php';
+require_once 'groups/verbes_en_ancien.php';  
+require_once 'groups/verbes_defectifs.php';
+//  require_once 'groups/irregular.php';
 ?>
-<style type="text/css">
-#menu, #rechts  {display:none;}
-#right.col-md-2{display:none;}	  
-#content.col-md-7 {width:85%;}
-	  
 
-@media 
-  (min-device-width: 800px) 
-  and (max-device-width: 1280px) {
-#menu.col-md-3 {width:23%;}
-#content.col-md-7 {width:77%;}
-}
-</style> 
-<p>Die Konjugation von <?=$verb ?></p>
-<p>Das Verb <b><?=$verb ?></b> wird <b><?=$regelmaessig ?></b> konjugiert und wird in den zusammengesetzten Zeiten mit den Hilfsverb <b><?=$auxiliaire ?></b> gebildet.</p>
-<p><b><?=$verb ?></b> ist ein <b><?=$reflexiv ?></b> und <b><?=$transitiv ?></b> Verb.</p>
+<p>Die Konjugation von <?=($_GET['verb']) ?></p>
+<p>Das Verb <b><?=($_GET['verb']) ?></b> wird <b><?=$regelmaessig ?></b> konjugiert und wird in den zusammengesetzten Zeiten mit den Hilfsverb <b><?=$auxiliaire ?></b> gebildet.</p>
+<p><b><?=($_GET['verb']) ?></b> ist ein <b><?=$reflexiv ?></b> und <b><?=$transitiv ?></b> Verb.</p>
 <?
-  include ($_SERVER['DOCUMENT_ROOT'] . "/data/languages/french/verbes_en_ancien.php");	// Array mit allen Verben aus der altfranzösischen Sprache
-  if (in_array($verb, $verbes_en_ancien)) {
-    echo '<p>'.$verb.' kommt aus der <b>altfranzösischen Sprache</b>.</p>';
-  }
-  include ($_SERVER['DOCUMENT_ROOT'] . "/data/languages/french/verbes_defectifs.php");	// Array mit allen defekten Verben    
-  if (in_array($verb, $verbes_defectifs)) {
-    echo '<p>'.$verb.' ist ein <b>defektes Verb</b>, d. h. es  bildet nicht alle Formen aus.</p>';
+  if (in_array($infinitiveVerb, $verbes_en_ancien)) {
+    echo '<p>'.$infinitiveVerb.' kommt aus der <b>altfranzösischen Sprache</b>.</p>';
   }  
-  if (in_array($verb, $impersonnels)) {
-    echo '<p>'.$verb.' ist ein <b>unpersönliches Verb</b>.</p>';	
+  if (in_array($infinitiveVerb, $verbes_defectifs)) {
+    echo '<p>'.$infinitiveVerb.' ist ein <b>defektes Verb</b>, d. h. es  bildet nicht alle Formen aus.</p>';
+  }  
+  if (in_array($infinitiveVerb, $impersonnels)) {
+    echo '<p>'.$infinitiveVerb.' ist ein <b>unpersönliches Verb</b>.</p>';	
   }
 ?> 
-<p><b><?=$verb ?></b> ist in <b><?=$gruppe ?></b>.</p>   
+<p><b><?=($_GET['verb']) ?></b> ist in <b><?=$gruppe ?></b>.</p>   
 <h2 class="home">Verzeichnis</h2>
 <ul style="list-style-type:none;">
 	<li><a class="down" href="#indicatif">Indicatif</a></li>
