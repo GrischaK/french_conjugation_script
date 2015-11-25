5ab769ef66537d635f25f0188b81046f884e6019
GrischaK <grischa.kosba@gmail.com>
2015-11-25 23:19:18 +0100

    added const RE and  I_TREMA_R

--- classes/EndingWith.php
+++ classes/EndingWith.php
@@ -1,7 +1,16 @@
 <?php
-class EndingWith extends Enum {
-	const ER = '-er'; // inclued -uer + -üer + ier - pailler
-	const IR = '-ir'; 
-		const OIR = '-oir'; // not ExceptionModel SEOIR !in_array($verb, $seoir) 
+
+class EndingWith extends Enum
+{
+
+    const ER = '-er';
+ // inclued -uer + -üer + ier + -iller + -éer
+    const IR = '-ir';
+
+    const OIR = '-oir';
+ // not ExceptionModel SEOIR !in_array($verb, $seoir)
+    const I_TREMA_R = '-ïr';
+
+    const RE = '-re';
 }
 ?>
