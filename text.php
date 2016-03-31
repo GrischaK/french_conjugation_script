7b999bb7cb55050272460acca055bfdc4fb33446
GrischaK <grischa.kosba@gmail.com>
2016-03-31 17:13:00 +0200

    deleted first echo for finding_auxiliaire and changed to UTF8

--- text.php
+++ text.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Hier findet man alles, was über den Konjugationstabellen steht.
+Hier findet man alles, was Ã¼ber den Konjugationstabellen steht.
 */ 
-$regelmaessig = in_array($infinitiveVerb, $unregelmaessige_verben) ? 'unregelmäßig' : 'regelmäßig'; 
+$regelmaessig = in_array($infinitiveVerb, $unregelmaessige_verben) ? 'unregelmÃ¤ÃŸig' : 'regelmÃ¤ÃŸig'; 
 $reflexiv = in_array($infinitiveVerb, $verbes_pronominaux) ? 'reflexives' : 'nicht reflexives';
 $conditions = [];
 if (in_array($infinitiveVerb, $verbes_transitifs)) {
@@ -19,10 +19,10 @@ $transitiv = implode(' oder ', $conditions);
 //$substr_er = array_filter($infinitiveVerb, function($value) { return substr($value, -2) == 'er'; });
 function finding_group(InfinitiveVerb $infinitiveVerb)
 {
-if ((substr ($infinitiveVerb, - 2, 2 ) == 'er') &&  (!in_array($infinitiveVerb,['aller','raller','re-aller','saisir-arrêter','saisir-brandonner','saisir-exécuter','s’en aller','sur-aller']))) {
+if ((substr ($infinitiveVerb, - 2, 2 ) == 'er') &&  (!in_array($infinitiveVerb,['aller','raller','re-aller','saisir-arrÃªter','saisir-brandonner','saisir-exÃ©cuter','sâ€™en aller','sur-aller']))) {
     $gruppe = 'der 1. Verbgruppe';
   } 
-  elseif ((substr ($infinitiveVerb, - 2, 2 ) == 'ir') or (in_array(mb_substr($infinitiveVerb, - 2, 2, "utf-8"), ["ïr"])) && !in_array($infinitiveVerb, $unregel_ir)) {
+  elseif ((substr ($infinitiveVerb, - 2, 2 ) == 'ir') or (in_array(mb_substr($infinitiveVerb, - 2, 2, "utf-8"), ["Ã¯r"])) && !in_array($infinitiveVerb, $unregel_ir)) {
     $gruppe = 'der 2. Verbgruppe';
   } 
   elseif ($infinitiveVerb == 'saisir-brandonner') {
@@ -45,26 +45,26 @@ require_once 'groups/verbes_defectifs.php';
 //  require_once 'groups/irregular.php';
 ?>
 
-<p>Die Konjugation von <?=($_GET['verb']) ?></p>
-<p>Das Verb <b><?=($_GET['verb']) ?></b> wird <b><?=$regelmaessig ?></b> konjugiert und wird in den zusammengesetzten Zeiten mit den Hilfsverb <b><?=$auxiliaire ?></b> gebildet.</p>
-<p><b><?=($_GET['verb']) ?></b> ist ein <b><?=$reflexiv ?></b> und <b><?=$transitiv ?></b> Verb.</p>
-<?
+<h2 class="home">Die Konjugation von <?php echo($_GET['verb']) ?></h2>
+<p>Das Verb <b><?php echo($_GET['verb']) ?></b> wird <b><?php echo$regelmaessig ?></b> konjugiert und wird in den zusammengesetzten Zeiten mit den Hilfsverb <b><?php echo finding_auxiliaire( new InfinitiveVerb($_GET['verb'])); ?></b> gebildet.</p>
+<p><b><?php echo($_GET['verb']) ?></b> ist ein <b><?php echo $reflexiv ?></b> und <b><?php echo $transitiv ?></b> Verb.</p>
+<?php
   if (in_array($infinitiveVerb, $verbes_en_ancien)) {
-    echo '<p>'.$infinitiveVerb.' kommt aus der <b>altfranzösischen Sprache</b>.</p>';
+    echo '<p>'.$infinitiveVerb.' kommt aus der <b>altfranzÃ¶sischen Sprache</b>.</p>';
   }  
   if (in_array($infinitiveVerb, $verbes_defectifs)) {
     echo '<p>'.$infinitiveVerb.' ist ein <b>defektes Verb</b>, d. h. es  bildet nicht alle Formen aus.</p>';
   }  
   if (in_array($infinitiveVerb, $impersonnels)) {
-    echo '<p>'.$infinitiveVerb.' ist ein <b>unpersönliches Verb</b>.</p>';	
+    echo '<p>'.$infinitiveVerb.' ist ein <b>unpersÃ¶nliches Verb</b>.</p>';	
   }
 ?> 
-<p><b><?=($_GET['verb']) ?></b> ist in <b><?=finding_group( new InfinitiveVerb($_GET['verb'])); ?></b>.</p>   
+<p><b><?php echo($_GET['verb']) ?></b> ist in <b><?php echo finding_group( new InfinitiveVerb($_GET['verb'])); ?></b>.</p>  
 <h2 class="home">Verzeichnis</h2>
 <ul style="list-style-type:none;">
 	<li><a class="down" href="#indicatif">Indicatif</a></li>
 	<li><a class="down" href="#subjonctif">Subjonctif</a></li>
 	<li><a class="down" href="#conditionnel">Conditionnel</a></li>
-	<li><a class="down" href="#imperatif">Impératif</a></li>
+	<li><a class="down" href="#imperatif">ImpÃ©ratif</a></li>
 	<li><a class="down" href="#modes-impersonnels">Modes impersonnels</a></li>
 </ul>
