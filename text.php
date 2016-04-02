58e3dc496cd9558559605256270d2de133318305
GrischaK <grischa.kosba@gmail.com>
2016-04-02 21:42:13 +0200

    add all arrays as global  for print_explanatory_text

--- text.php
+++ text.php
@@ -1,17 +1,15 @@
 <?php
-/*
-Hier findet man alles, was über den Konjugationstabellen steht.
-*/ 
 function print_explanatory_text(InfinitiveVerb $infinitiveVerb) {
-	require_once 'classes/InfinitiveVerb.php';
-	require_once 'verbs.php';
-	include 'groups/verbes_pronominaux.php';
-	require_once 'groups/verbes_exclusivement_pronominaux.php';
-	require_once 'groups/verbes_intransitifs.php';
-	require_once 'groups/verbes_transitifs.php';
-	
-	require_once 'groups/verbes_en_ancien.php';
-	require_once 'groups/verbes_defectifs.php';
+global $verbes_en_ancien,$verbes_defectifs,$impersonnels,$verbes_transitifs,$verbes_intransitifs,$verbes_pronominaux,$unregelmaessige_verben;
+require_once 'classes/InfinitiveVerb.php';
+require_once 'verbs.php';
+require_once 'groups/verbes_pronominaux.php';
+require_once 'groups/verbes_exclusivement_pronominaux.php';
+require_once 'groups/verbes_intransitifs.php';
+require_once 'groups/verbes_transitifs.php';
+require_once 'groups/irregular-verb-groups.php';
+require_once 'groups/verbes_en_ancien.php';
+require_once 'groups/verbes_defectifs.php';
 	
 $regelmaessig = in_array($infinitiveVerb, $unregelmaessige_verben) ? 'unregelmäßig' : 'regelmäßig'; 
 $reflexiv = in_array($infinitiveVerb, $verbes_pronominaux) ? 'reflexives' : 'nicht reflexives';
@@ -39,8 +37,6 @@ if ((substr ($infinitiveVerb, - 2, 2 ) == 'er') &&  (!in_array($infinitiveVerb,[
   else { // aller,....
     $group = 'der 3. Verbgruppe';
   }
-
-
 echo '<h2 class="home">Die Konjugation von '.$_GET['verb'].'</h2>
 <p>Das Verb <b>'.$_GET['verb'].'</b> wird <b>'.$regelmaessig.
 '</b> konjugiert und wird in den zusammengesetzten Zeiten mit den Hilfsverb <b>'.
