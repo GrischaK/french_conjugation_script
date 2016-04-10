877a7c1ae4da0f5d24664232bb6b62d483974362
GrischaK <grischa.kosba@gmail.com>
2016-04-10 23:57:02 +0200

    deleted empty lines

--- classes/EndingWith.php
+++ classes/EndingWith.php
@@ -1,15 +1,10 @@
 <?php
 class EndingWith extends Enum
 {
-
-    const ER = '-er';
- // inclued -uer + -üer + ier + -iller + -éer
+    const ER = '-er';// included -uer + -üer + ier + -iller + -éer
     const IR = '-ir';
-
-    const OIR = '-oir';
- // not ExceptionModel SEOIR !in_array($verb, $seoir)
+    const OIR = '-oir';// not ExceptionModel SEOIR !in_array($verb, $seoir)
     const I_TREMA_R = '-ïr';
-
     const RE = '-re';
 }
 ?>
