0717cf1e4f40f4825ead2592459d38821b8bff55
GrischaK <grischa.kosba@gmail.com>
2016-04-10 23:56:49 +0200

    add voice parameter

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,8 +1,8 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
-	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood) {
+	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood, Voice $voice) {
 		$infinitiveVerb = new InfinitiveVerb($auxiliaire->getInfinitiveString());
-		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood);
+		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood, $voice);
 		$this->initialize ( $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person );
 	}
 }
