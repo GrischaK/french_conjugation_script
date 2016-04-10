0b9d1655eecda70b8fc4a5e349ca9b92220ff992
GrischaK <grischa.kosba@gmail.com>
2016-04-10 23:57:42 +0200

    add new parameter voice

--- tests/AllerTest.php
+++ tests/AllerTest.php
@@ -4,8 +4,8 @@ class AllerTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider AllerTestProvider
 	 */
-	public function testAller($expectedResult, $person,$tense,$mood) {
-		$this->assertEquals ( $expectedResult, aller(new Person($person), new Tense($tense), new Mood($mood)));
+	public function testAller($expectedResult, $person,$tense,$mood, $voice) {
+		$this->assertEquals ( $expectedResult, aller(new Person($person), new Tense($tense), new Mood($mood), new Voice($voice)));
 	}
 	public function AllerTestProvider() {
 		return [
@@ -14,36 +14,42 @@ class AllerTest extends PHPUnit_Framework_TestCase {
 						'FirstPersonSingular',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				],
 				[
 						'vas',
 						'SecondPersonSingular',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				],
 				[
 						'va',
 						'ThirdPersonSingular',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				],
 				[
 						'allons',
 						'FirstPersonPlural',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				],
 				[
 						'allez',
 						'SecondPersonPlural',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				],
 				[
 						'vont',
 						'ThirdPersonPlural',
 						'Futur_compose',
 						'Indicatif',
+						Voice::Active,						
 				]
 		];
 	}
