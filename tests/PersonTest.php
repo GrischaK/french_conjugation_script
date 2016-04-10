9355c0c94b62bf3d8960169ab7bf84c2850c9290
GrischaK <grischa.kosba@gmail.com>
2016-04-10 02:00:33 +0200

    add new parameter gender + feminine test results

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -4,69 +4,93 @@ class PersonTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider PersonTestProvider
 	 */
-	public function testPerson($expectedResult, $person, $mood) {
-		$this->assertEquals ( $expectedResult, personal_pronoun(new Person($person), new Mood($mood)));
+	public function testPerson($expectedResult, $person, $gender, $mood) {
+		$this->assertEquals ( $expectedResult, personal_pronoun(new Person($person), new Gender($gender), new Mood($mood)));
 	}
 	public function PersonTestProvider() {
 		return [
 				[
 						'je',
 						'FirstPersonSingular',
-						'Indicatif',
+						'Masculine',						
+						'Indicatif',						
 				],
 				[
 						'tu',
 						'SecondPersonSingular',
+						'Masculine',
 						'Indicatif',
 				],
 				[
 						'il',
 						'ThirdPersonSingular',
+						'Masculine',
 						'Indicatif',
 				],
 				[
+						'elle',
+						'ThirdPersonSingular',
+						'Feminine',
+						'Indicatif',
+				],				
+				[
 						'nous',
 						'FirstPersonPlural',
+						'Masculine',
 						'Indicatif',
 				],
 				[
 						'vous',
 						'SecondPersonPlural',
+						'Masculine',						
 						'Indicatif',
 				],
 				[
 						'ils',
 						'ThirdPersonPlural',
+						'Masculine',						
 						'Indicatif',
 				],
 				[
+						'elles',
+						'ThirdPersonPlural',
+						'Feminine',
+						'Indicatif',
+				],					
+				[
 						'que je',
 						'FirstPersonSingular',
+						'Masculine',						
 						'Subjonctif',
 				],
 				[
 						'que tu',
 						'SecondPersonSingular',
+						'Masculine',						
 						'Subjonctif',
 				],
 				[
 						'qu’il',
 						'ThirdPersonSingular',
+						'Masculine',						
 						'Subjonctif',
 				],
 				[
 						'que nous',
 						'FirstPersonPlural',
+						'Masculine',						
 						'Subjonctif',
 				],
 				[
 						'que vous',
 						'SecondPersonPlural',
+						'Masculine',						
 						'Subjonctif',
 				],
 				[
 						'qu’ils',
 						'ThirdPersonPlural',
+						'Masculine',						
 						'Subjonctif',
 				]
 				
