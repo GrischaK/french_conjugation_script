0e9788d2a87917e935d41c79c8f0bb917149e1af
GrischaK <grischa.kosba@gmail.com>
2016-04-13 16:12:48 +0200

    added new parameter $sentencetype

--- classes/ConjugatedAuxiliaireVerb.php
+++ classes/ConjugatedAuxiliaireVerb.php
@@ -1,8 +1,8 @@
 <?php
 class ConjugatedAuxiliaireVerb extends ConjugatedVerb {
-	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood, Voice $voice) {
+	public function __construct(Auxiliaire $auxiliaire, Person $person, Tense $tense, Mood $mood, Voice $voice, SentenceType $sentencetype) {
 		$infinitiveVerb = new InfinitiveVerb($auxiliaire->getInfinitiveString());
-		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood, $voice);
+		$conjugatedVerb = conjugated_auxiliaire($auxiliaire, $person, $tense, $mood, $voice, $sentencetype);
 		$this->initialize ( $conjugatedVerb, $infinitiveVerb, $mood, $tense, $person );
 	}
 }
