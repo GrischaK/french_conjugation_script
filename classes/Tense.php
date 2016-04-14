180f17a4c211c930028e63715b737f7704b92d03
GrischaK <grischa.kosba@gmail.com>
2016-04-14 22:42:09 +0200

    add new Tense

--- classes/Tense.php
+++ classes/Tense.php
@@ -12,7 +12,8 @@ class Tense extends Enum {
 	const Passe_anterieur = 'passe_anterieur';
 	const Futur_anterieur = 'futur_anterieur';	// (Futur II)
 	const Futur_compose = 'futur_compose'; 	// (Futur proche)
-
+	const Passe_recent = 'passe_recent'; 	// (Passé proche)
+	
 	const Premiere_Forme = 'premiere_Forme';
 	const Deuxieme_Forme = 'deuxieme_Forme';
 }
