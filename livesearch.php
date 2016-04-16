31da678caea9a67269a376e114c5bb0ca46afe5d
GrischaK <grischa.kosba@gmail.com>
2016-04-16 19:58:27 +0200

    now is the live search working

--- livesearch.php
+++ livesearch.php
@@ -1,31 +1,27 @@
 <?php
-	header('Content-Type: application/xml; charset=utf-8');
-	require_once 'verbs.php';
+header ( 'Content-Type: application/xml; charset=utf-8' );
+require_once 'verbs.php';
 ?>
-<? echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<? echo '<?xml version="1.0" encoding="UTF-8"?>'?>
 <pages>
 <?php
-	$num=0;
-	$hint="";
-if (is_array($infinitiveVerb) || is_object($infinitiveVerb))
-{	
-	foreach($infinitiveVerb as $buchstabe){
-		if (is_array($buchstabe) || is_object($buchstabe))	
-		{	
-			foreach($buchstabe as $verb){
-				if(preg_match("/^".$_GET['pattern']."/",$infinitiveVerb)){
-					$hint.="<a class=\"franzoesisch konju\" href='".substr($verb,0,1)."/".$verb."/'>".$verb."</a>";
-					$num++;
-				}
-				if($num>10)break;
-			}
-			if($num>10)break;
+$num = 0;
+$hint = "";
+foreach ( $infinitiveVerb as $buchstabe ) {
+	foreach ( $buchstabe as $infinitiveVerb ) {
+		if (preg_match ( "/^" . $_GET ['pattern'] . "/", $infinitiveVerb )) {
+			$hint .= "<a class=\"franzoesisch konju\" href='" . substr ( $infinitiveVerb, 0, 1 ) . "/" . $infinitiveVerb . "/'>" . $infinitiveVerb . "</a>";
+			$num ++;
 		}
-	}		
-}	
-	if($num==0)
-		echo 'keine Vorschläge';
-	else 
-		echo $hint;
+		if ($num > 10)
+			break;
+	}
+	if ($num > 10)
+		break;
+}
+if ($num == 0)
+	echo 'keine Vorschläge';
+else
+	echo $hint;
 ?>
 </pages>
