0baf768291309d9dce9410bb7c82d8f1f2f38d2f
GrischaK <grischa.kosba@gmail.com>
2016-04-20 13:19:32 +0200

    add new results

--- tests/IsCompositeTest.php
+++ tests/IsCompositeTest.php
@@ -8,23 +8,103 @@ class CompositeTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ( $expectedResult, isComposite(new Mood($mood),new Tense($tense)));
 	}
 	public function CompsoiteTestProvider() {
-		return [
+		return [   
 				[
 						true,
-						'Futur_compose',
-						'Indicatif',
-				],				
+						Tense::Passe_compose,
+						Mood::Indicatif,
+				],	
 				[
 						true,
-						'Passe',
-						'Imperatif',
+						Tense::Plus_que_parfait,
+						Mood::Indicatif,
+				],
+				[
+						true,
+						Tense::Passe_anterieur,
+						Mood::Indicatif,
+				],	
+				[
+						true,
+						Tense::Futur_anterieur,
+						Mood::Indicatif,
+				],					
+				[
+						true,
+						Tense::Futur_compose,
+						Mood::Indicatif,
+				],			
+				[
+						true,
+						Tense::Passe_recent,
+						Mood::Indicatif,
+				],	
+				[
+						true,
+						Tense::Plus_que_parfait,
+						Mood::Subjonctif,
+				],	
+				[
+						true,
+						Tense::Passe,
+						Mood::Subjonctif,
+				],
+				[
+						true,
+						Tense::Premiere_Forme,
+						Mood::Conditionnel,
+				],
+				[
+						true,
+						Tense::Deuxieme_Forme,
+						Mood::Conditionnel,
+				],					
+				[
+						true,
+						Tense::Passe,
+						Mood::Imperatif,
+				],
+				
+				[
+						false,
+						Tense::Present,
+						Mood::Indicatif,
+				],	
+				[
+						false,
+						Tense::Imparfait,
+						Mood::Indicatif,
 				],
 				[
 						false,
-						'Present',
-						'Indicatif',
-				],				
-				
+						Tense::Passe,
+						Mood::Indicatif,
+				],	
+				[
+						false,
+						Tense::Futur,
+						Mood::Indicatif,
+				],
+				[
+						false,
+						Tense::Present,
+						Mood::Subjonctif,
+				],	
+				[
+						false,
+						Tense::Imparfait,
+						Mood::Subjonctif,
+				],
+				[
+						false,
+						Tense::Present,
+						Mood::Conditionnel,
+				],					
+				[
+						false,
+						Tense::Present,
+						Mood::Imperatif,
+				],					
 		];
 	}
 }
